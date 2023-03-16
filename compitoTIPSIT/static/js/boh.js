0ee4e6be65356e37c0e667f048618a0bf85f0166
Saveriopetaroscia05 <saveriopetaroscia40@gmail.com>
2023-03-16 09:31:56 +0100

    triss

--- compitoTIPSIT/static/js/boh.js
+++ compitoTIPSIT/static/js/boh.js
@@ -12,14 +12,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const PLAYER_O = 'PLAYER_O';
     const PAREGGIO = 'PAREGGIO';
 
-
-    /*
-        Indexes within the board
-        [0] [1] [2]
-        [3] [4] [5]
-        [6] [7] [8]
-    */
-
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
